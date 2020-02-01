09b32a5d4fcabdcd4e1931bd8fdd751e8f259a39
emine <eminesa34@gmail.com>
2020-02-01 19:31:29 +0300

    First commit for start

---
+++ app/src/main/java/com/example/pharmacyapp/ui/listactivity/ListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pharmacyapp.ui.listactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pharmacyapp.R
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
---
+++ app/src/main/java/com/example/pharmacyapp/ui/mainactivity/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.pharmacyapp.ui.mainactivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.pharmacyapp.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
